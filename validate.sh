e5a3f94a678210bd96ee2415771405ed9e1be684
Pierre Zurmely <pzurmely@amadeus.com>
2014-04-22 17:25:32 +0200

    Validation script simplified to avoid issue with the site used

--- validate.sh
+++ validate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-curl -s -F "uploaded_file=@illuminatedTexts.html;type=text/html" http://validator.w3.org/check -D validation_summary.txt -o validation_result.html
-sed -i 's|@import "\./style|@import "http://validator.w3.org/style|g; s|img src="images|img src="http://validator.w3.org/images|g' validation_result.html
+#curl -s -F "uploaded_file=@illuminatedTexts.html;type=text/html" http://validator.w3.org/check -D validation_summary.txt -o validation_result.html
+#sed -i 's|@import "\./style|@import "http://validator.w3.org/style|g; s|img src="images|img src="http://validator.w3.org/images|g' validation_result.html
 
-validation_status=$(awk '/X-W3C-Validator-Status/ {print $2}' validation_summary.txt)
+#validation_status=$(awk '/X-W3C-Validator-Status/ {print $2}' validation_summary.txt)
 
 #case $validation_status in
 #    Valid?) exit 0;;
